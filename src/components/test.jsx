db5d54279f9272c5eb4695780fcb01072997ba96
Max Fedak <max.fedak@gmail.com>
2022-11-19 12:56:03 +0200

    test

---
+++ src/components/test.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export const Table = () => {
+      return (
+         <table>
+            <tr>
+               <th>Test</th>
+               <th>Test2</th>
+               <th>Test3</th>
+            </tr>
+            <tr>
+               <td>001</td>
+               <td>002</td>
+               <td>003</td>
+            </tr>
+            <tr>
+               <td>001</td>
+               <td>002</td>
+               <td>003</td>
+            </tr>
+            <tr>
+               <td>001</td>
+               <td>002</td>
+               <td>003</td>
+            </tr>
+         </table>
+      )
+}
